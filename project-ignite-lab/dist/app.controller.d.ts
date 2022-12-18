0caa02bfc889c3c5b71f38d1899e4f46ac1e3d62
@a.d.j.a.n.e <adjanead1@gmail.com>
2022-12-18 15:25:48 -0300

    Delete project-ignite-lab directory

--- project-ignite-lab/dist/app.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaService } from './prisma.service';
-export declare class AppController {
-    private readonly prisma;
-    PrismaService: any;
-    constructor(prisma: PrismaService);
-    getHello(): string;
-}
--- project-ignite-lab/dist/prisma.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { INestApplication, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
-export declare class PrismaService extends PrismaClient implements OnModuleInit {
-    notification: any;
-    onModuleInit(): Promise<void>;
-    enableShutdownHooks(app: INestApplication): Promise<void>;
-}
